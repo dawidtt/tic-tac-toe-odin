ab02c95b8e211d51fbf2631f3cb3cdc095da9ab8
dawidtt <dawid.gorszka@gmail.com>
2024-09-02 20:05:29 +0200

    Add available moves

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const game = (function () {
+  let movesAvailable = 9;
   let gameboard = [
     [".", ".", "."],
     [".", ".", "."],
     [".", ".", "."],
   ];
-  const makeMove = (row, col, move) => (gameboard[row][col] = move);
+  const makeMove = (row, col, move) => {
+    gameboard[row][col] = move;
+    movesAvailable--;
+    return move;
+  };
   return { gameboard, makeMove };
 })();
 function createPlayer(name, move) {
