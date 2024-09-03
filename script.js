bf043c61ee0c510591470a61d623979d3ad9c92f
dawidtt <dawid.gorszka@gmail.com>
2024-09-03 18:33:18 +0200

    Create lookForWinner function to check if there is winner after every round

--- script.js
+++ script.js
@@ -14,22 +14,52 @@ const gameControl = (function () {
   const getNumberOfMoves = () => movesAvailable;
 
   const makeMove = (row, col, player) => {
-    const message = false;
+    let message = false;
     if (gameboard.gameboard[row][col] === ".") {
       gameboard.gameboard[row][col] = player.move;
       decreaseNumberOfMoves();
     } else {
       message = "This field is already taken";
     }
-    return message ? message : player.move;
+    displayGameboard();
+    message = lookForWinner();
+    return message
+      ? message
+      : `${player.name} placed ${player.move} on ${row} row in ${col} column`;
+  };
+  const lookForWinner = () => {
+    let winPlayerMove;
+    let winner = false;
+    let winnerMessage = "";
+    const tempGameboardArray = gameboard.gameboard;
+    for (const gameboardRow of tempGameboardArray) {
+      if (
+        gameboardRow[0] === gameboardRow[1] &&
+        gameboardRow[1] === gameboardRow[2] &&
+        gameboardRow[2] !== "."
+      ) {
+        winPlayerMove = gameboardRow[0];
+        for (const player of players) {
+          if (player.move === winPlayerMove) {
+            winner = player.name;
+            winnerMessage = `${winner} won the game`;
+          }
+        }
+        break;
+      }
+    }
+    return winner ? winnerMessage : winner;
   };
   const displayGameboard = () => {
     console.log(gameboard.gameboard);
   };
-  return { makeMove, getNumberOfMoves, displayGameboard };
+  return { makeMove, getNumberOfMoves, displayGameboard, lookForWinner };
 })();
+
 function createPlayer(name, move) {
   return { name, move };
 }
+
 const josh = createPlayer("josh", "O");
 const mike = createPlayer("mike", "X");
+const players = [josh, mike];
